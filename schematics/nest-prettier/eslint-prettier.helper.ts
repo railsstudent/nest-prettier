1cb4b36f0411758bd50f62b5773e335581a9a13f
Connie Leung <cleung334@gmail.com>
2022-07-04 01:29:20 +0800

    chore: commented out that add rule to .eslintrc.js. Too difficult

--- schematics/nest-prettier/eslint-prettier.helper.ts
+++ schematics/nest-prettier/eslint-prettier.helper.ts
@@ -25,7 +25,10 @@ export function addEslintPrettier(options: Schema): Rule {
     } else if (options.eslintFileFormat === ESLINT_FILE_FORMAT.JSON) {
       updateEslintJson(tree, buffer, configFileName, prettierRule)
     } else if (options.eslintFileFormat === ESLINT_FILE_FORMAT.JAVASCRIPT) {
-      updateEslintJs(tree, configFileName, prettierRule)
+      context.logger.info('Does not support JavaScript')
+      context.logger.info(`Append 'plugin:prettier/recommended' to extends array in ${configFileName}`)
+      context.logger.info(`Append ${JSON.stringify(prettierRule)} to rules object in ${configFileName}`)
+      // updateEslintJs(tree, buffer, configFileName, prettierRule)
     }
 
     context.logger.info(`Added eslint-plugin-prettier`)
@@ -35,8 +38,9 @@ export function addEslintPrettier(options: Schema): Rule {
 
 function updateEslintConfig(tree: Tree, buffer: Buffer, configFileName: string, prettierRule: (string | PrettierOptions)[]) {
   const packageJson = JSON.parse(buffer.toString())
-  const { eslintConfig = {} } = packageJson.eslintConfig || {}
+  const eslintConfig = packageJson.eslintConfig || {}
   const { extends: eslintExtends = [], rules = {} } = eslintConfig
+
   packageJson.eslintConfig = {
     ...eslintConfig,
     extends: uniq([...eslintExtends, 'plugin:prettier/recommended']),
@@ -59,8 +63,75 @@ function updateEslintJson(tree: Tree, buffer: Buffer, configFileName: string, pr
 }
 
 // function updateEslintJs(tree: Tree, buffer: Buffer, configFileName: string, prettierRule: (string | PrettierOptions)[]) {
-//   const eslintJson = JSON.parse(buffer.toString())
-//   // eslintJson.extends = [...eslintJson.extends, 'plugin:prettier/recommended']
-//   // eslintJson.rules = { ...eslintJson.rules, 'prettier/prettier': prettierRule }
-//   // tree.overwrite(configFileName, JSON.stringify(eslintJson, null, 2))
+//   const strBuffer = buffer.toString()
+//   const strippedContent = strBuffer.replace('module.exports =', '').trim()
+//   const replacedQuotesContent = strippedContent.replace(new RegExp("'", 'g'), '"')
+//   const dirname = '__dirname,'
+
+//   const lines = replacedQuotesContent.split(/\r?\n/)
+//   const jsonifyLines: string[] = []
+//   for (let i = 0; i < lines.length; i++) {
+//     const { key, value } = tokenizeKeyValue(i, lines)
+
+//     if (value) {
+//       console.log('value', value)
+//       const isSingleQuoted = key[0] === "'" && key[key.length - 1] === "'"
+//       const quotedKey = isSingleQuoted ? `"${key.substring(1, key.length - 1)}"` : key
+//       const isDoubleQuoted = quotedKey[0] === '"' && quotedKey[quotedKey.length - 1] === '"'
+//       const doubleQuotedKey = isDoubleQuoted ? quotedKey : `"${quotedKey}"`
+//       const specialValue = value === dirname ? `"${value.substring(0, value.length - 1)}",` : value
+//       jsonifyLines.push(`${doubleQuotedKey}: ${specialValue}`)
+//     } else {
+//       const lastLine = jsonifyLines.pop()
+//       if (lastLine) {
+//         const lineStrippedTrailComma = lastLine.endsWith(',') ? lastLine.substring(0, lastLine.length - 1) : lastLine
+//         jsonifyLines.push(lineStrippedTrailComma)
+//       }
+//       jsonifyLines.push(key)
+//     }
+//   }
+
+//   console.log(jsonifyLines)
+
+//   const eslintJson = JSON.parse(jsonifyLines.join('\r\n'))
+//   eslintJson.extends = uniq([...eslintJson.extends, 'plugin:prettier/recommended'])
+//   eslintJson.rules = {
+//     ...eslintJson.rules,
+//     'prettier/prettier': prettierRule,
+//   }
+
+//   console.log(eslintJson)
+
+//   const newEslintModuleExport = `module.exports = ${JSON.stringify(eslintJson, null)}`.replace('"__dirname",', dirname)
+//   console.log('newEslintModuleExport', newEslintModuleExport)
+//   tree.overwrite(configFileName, newEslintModuleExport)
+// }
+
+// function tokenizeKeyValue(idxLine: number, lines: string[]) {
+//   const line = lines[idxLine]
+//   const idxColon = line.indexOf(':')
+//   const objKey = idxColon >= 0 ? line.substring(0, idxColon).trim() : line.trim()
+//   const objValue = idxColon >= 0 ? line.substring(idxColon + 1).trim() : undefined
+
+//   let strippedKey = objKey
+//   let strippedValue = objValue
+//   if (idxLine === lines.length - 2) {
+//     if (strippedValue) {
+//       if (strippedValue.endsWith(',')) {
+//         strippedValue = strippedValue.substring(0, strippedValue.length - 1)
+//       }
+//     } else {
+//       if (strippedKey.endsWith(',')) {
+//         strippedKey = strippedKey.substring(0, strippedKey.length - 1)
+//       }
+//     }
+//   } else if (idxLine === lines.length - 1) {
+//     if (strippedKey.endsWith(';')) {
+//       strippedKey = strippedKey.substring(0, strippedKey.length - 1)
+//     }
+//   }
+//   return {
+//     key: strippedKey,
+//     value: strippedValue,
+//   }
 // }
